907e123ea74fa4ef03f3f2a6650ea820d46fae7e
Serina <serina.chhem@gmail.com>
2021-01-18 19:35:24 +0100

    Revert "Added Headroom for header"
    This reverts commit 515b636d104fb65efb19a2fe3e5b8731063d512b.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-import Header from './components/Header'
 
 const Dictaphone = () => {
   const { transcript, resetTranscript } = useSpeechRecognition()
@@ -10,9 +9,7 @@ const Dictaphone = () => {
   }
 
   return (
-    
     <div>
-      <Header/>
       <button onClick={SpeechRecognition.startListening({ language: 'fr-FR' })}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
